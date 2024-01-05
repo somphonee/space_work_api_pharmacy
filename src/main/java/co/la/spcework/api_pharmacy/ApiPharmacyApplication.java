00f30d946c83794cee3cbb63829737226d5a8d24
Somphone_xieng <sp.xayyalath@gmail.com>
2024-01-05 22:11:19 +0700

    first commit

---
+++ src/main/java/co/la/spcework/api_pharmacy/ApiPharmacyApplication.java
@@ -0,0 +1,13 @@
+package co.la.spcework.api_pharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPharmacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/la/spcework/api_pharmacy/ApiPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package co.la.spcework.api_pharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
